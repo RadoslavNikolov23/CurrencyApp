ab40335c7958414505a9b1d3b17ba7303da985ca
Rado <sitcom93@abv.bg>
2026-01-18 11:54:50 +0200

    Improve loading/error handling and button state in currency app
     Added loading and error state management to handle fetch status and errors.
    Updated UI to show loading and error messages and disable/hide the toggle button.

--- Currency.Client/src/App.jsx
+++ Currency.Client/src/App.jsx
@@ -2,22 +2,36 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-    const [currencies, SetCurrencies] = useState([]);
+    const [currencies, setCurrencies] = useState([]);
     const [showCurrencies, setShowCurrencies] = useState(true);
 
-useEffect(() => {
-    fetch("https://localhost:7279/api/currency")
-        .then(response => response.json())
-        .then(data => SetCurrencies(data))
-        .catch(error => console.error("Error fetching currencies:", error));
-}, []);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+       // setLoading(true);
+      //  setError(null);
+
+        fetch("https://localhost:7279/api/currenc")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to load currencies");
+                }
+
+                return response.json()
+            })
+            .then(data => setCurrencies(data))
+            .catch(error => setError(error.message))
+            .finally(()=>setLoading(false));
+    }, []);
 
 
     return (
         <div className="app">
             <h1 className="title">Currency App</h1>
 
-            {!showCurrencies && (
+            {!loading && !error && !showCurrencies && (
                 <ul className="currency-list">
                     {currencies.map(currency => (
                         <CurrencyInfo
@@ -30,9 +44,19 @@ useEffect(() => {
                 </ul>
             )}
 
-            <button className="button" onClick={() => setShowCurrencies(!showCurrencies)}>
+            <button
+                className="button"
+                onClick={() => setShowCurrencies(!showCurrencies)}
+                disabled={loading}
+                style={{ display: error ? "none" : "block" }}
+            >
                 {showCurrencies ? "Show currencies" : "Hide currencies"}
             </button>
+
+            {loading && <p>Loading...</p>}
+
+            {error && <p className="error">{error}</p>}
+
         </div>
     );
 }
